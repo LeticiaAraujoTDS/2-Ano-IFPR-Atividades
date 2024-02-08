7e309f383f723eaa8d51b0cba192a1dc56d9925a
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-08 15:32:07 -0300

    Update and rename at1 to at1.php

---
+++ 1 Bimestre /Orientação-a-Objetos /at1.php
@@ -0,0 +1,23 @@
+<?php
+
+        $a = 1;
+
+        while ($a <= 10) {
+            print "$a \n";
+            $a++;
+        }
+        
+        $a = 1;
+
+        do {
+            print "$a \n";
+            $a++;
+        } while ($a <= 10);
+        
+
+        for ($i=1; $i < 11; $i++) { 
+           print "$i \n";
+        }
+
+    
+
