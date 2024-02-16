814a384efc707dbdf90bccadebe3d7d28f68902b
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-16 13:42:56 -0300

    Create at1_aula2.php
    Atividade da aula 2, realizada no dia 16/02/24

---
+++ 1 Bimestre /Orientação-a-Objetos /at1_aula2.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    for ($i=0; $i < 10; $i++) { 
+
+        $numeroInt = readline("Informe um número: ");
+
+            if ($numeroInt < 0 ) {
+
+                print "O número $numeroInt é negativo\n";
+
+                } else {
+
+                    print "O número $numeroInt é positivo.\n";
+                    
+                }
+    }
+
+   
