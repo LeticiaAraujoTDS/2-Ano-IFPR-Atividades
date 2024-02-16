d7e02eceeaddbf0652ee06bfb86be08ec464a4a2
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-16 15:28:28 -0300

    Update at_3_aula2.php

--- 1 Bimestre /Orientação-a-Objetos /aula 2 (16-02)/at_3_aula2.php
+++ 1 Bimestre /Orientação-a-Objetos /aula 2 (16-02)/at_3_aula2.php
@@ -2,12 +2,13 @@
 
     for ($i=0; $i < 5; $i++) { 
         
-        $nome = readline("Qual o seu nome? ");
-        $altura = readline("Qual a sua altura em metros? ");
-        $peso = readline("Qual o seu peso em quilos? ");
+        $nome = readline("Informe o seu nome: ");
+        $altura = (float)readline("Informe a sua altura (m): ");
+        $peso = (int)readline("Informe o seu peso (kg): ");
         $imc = $peso / ($altura * $altura);
-            print ("\n$nome tem peso $peso kg, altura de $altura m e imc de $imc. \n");
-            print "\n";
+           // print ("\n$nome pesa $peso kg, tem altura de $altura m e imc de $imc. \n");
+        printf("%s pesa %d, tem altura de %f e tem IMC de %.2f \n", $nome, $peso, $altura, $imc);
+            //%s = string ; %d = int ; %f = float ; PARA DEFINIR CASAS DECIMAIS É SÓ % .NUMERO DE CASASf
 
     }
     
