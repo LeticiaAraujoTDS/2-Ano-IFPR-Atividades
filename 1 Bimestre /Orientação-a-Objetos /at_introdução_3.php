b53d278f155cfb79a3a36603a8b6cbffdf8034d9
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-16 14:06:13 -0300

    Update at_introdução_3.php

--- 1 Bimestre /Orientação-a-Objetos /at_introdução_3.php
+++ 1 Bimestre /Orientação-a-Objetos /at_introdução_3.php
@@ -2,9 +2,7 @@
 
     for ($i=1980; $i <=2024; $i++) { 
         
-        if ($i % 4 == 0 and $i % 100 !== 0 or $i % 4 == 0 and $i % 400 == 0) {
-            print "O ano $i é bissexto.\n";
+        if (($i % 4 == 0 and $i % 100 !== 0) or ($i % 4 == 0 and $i % 400 == 0)) {//tem que proteger os and do or com parenteses
+            print "O ano $i é bissexto\n";
         }
-
-        
     }
