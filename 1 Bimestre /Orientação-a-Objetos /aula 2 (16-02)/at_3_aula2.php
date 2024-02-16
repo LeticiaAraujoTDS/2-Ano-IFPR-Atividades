7cea239630af8d8dee7966ae5f3d5e966677ef01
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-16 14:49:04 -0300

    Update at_3_aula2.php

--- 1 Bimestre /Orientação-a-Objetos /aula 2 (16-02)/at_3_aula2.php
+++ 1 Bimestre /Orientação-a-Objetos /aula 2 (16-02)/at_3_aula2.php
@@ -1,12 +1,14 @@
 <?php
 
-    $nome = readline("Qual o seu nome? ");
-    print "\n";
-    $altura = (int) readline("Qual a sua altura em metros? ");
-    print "\n";
-    $peso = (int) readline("Qual o seu peso em quilos? ");
-    print "\n";
+    for ($i=0; $i < 5; $i++) { 
+        
+        $nome = readline("Qual o seu nome? ");
+        $altura = readline("Qual a sua altura em metros? ");
+        $peso = readline("Qual o seu peso em quilos? ");
+        $imc = $peso / ($altura * $altura);
+            print ("\n$nome tem peso $peso kg, altura de $altura m e imc de $imc. \n");
+            print "\n";
 
-    $imc = $peso / ($altura * $altura);
-
-    print ("$nome tem peso $peso kg, altura de $altura m e imc de $imc. \n");
+    }
+    
+    
