4326d7794fe3355c10aa8b3859eeb285af678cf2
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-21 22:11:28 -0300

    Update exe_6.php

--- Orientação-a-Objetos /1 Bimestre /aula 2 (16-02-24)/exe_6.php
+++ Orientação-a-Objetos /1 Bimestre /aula 2 (16-02-24)/exe_6.php
@@ -2,14 +2,12 @@
 
     $a = readline("Informe o valor do coeficiente a: ");
     $b = readline("Informe o valor do coeficiente b: ");
-    $c = $a;
-
-    $n = 0;
+    $c = 0;
 
     for ($i=1; $i <= $b; $i++) { 
         
-        $c = $n +$a;
-        $n = $c;
+        $c += + $a;
+ 
     }
 
     print ("$c");
