3e9951fd467aa49a9f5e1b0041d647c203bdbd9e
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-02-21 21:51:28 -0300

    Update exe_3.php

--- Orientação-a-Objetos /1 Bimestre /aula 2 (16-02-24)/exe_3.php
+++ Orientação-a-Objetos /1 Bimestre /aula 2 (16-02-24)/exe_3.php
@@ -4,4 +4,11 @@
     $b = readline("Informe o valor de b: ");
     $c = readline("Informe o valor de c: ");
 
+    if ($a < $b && $a < $c) {
+        print "O " . $a . " é o menor número.";
+    } elseif ($b < $a && $b < $c ) {
+        print "O " . $b . " é o menor número.";
+    } else {
+        print "O " . $c . " é o menor número.";
+    }
     
