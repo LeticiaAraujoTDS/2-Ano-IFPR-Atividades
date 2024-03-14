11477773947ec77ebbac815721323c35d751b18d
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-03-14 14:18:55 -0300

    Create primos.php

---
+++ Orientação-a-Objetos /1 Bimestre /aula 6 (14-03-24)/primos.php
@@ -0,0 +1,31 @@
+<?php
+//Funcão
+function verificar($num)
+{
+    if ($num <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= ($num) / 2; $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//Programa Principal
+$vetor_num = array();
+while (true) {
+    $num = readline("Informe um número: ");
+    if ($num <= 0) {
+        print "Programa encerrado.\n";
+        print "Os números primos são: \n";
+        foreach ($vetor_num as $resultados) {
+            print $resultados . "\n";
+        }
+        break;
+    }
+    if (verificar($num)) {
+        array_push($vetor_num, $num);
+    }
+}
