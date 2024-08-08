059727308a40156df008bdf209cd4cee77d4dabf
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2024-08-08 13:29:41 -0300

    Add files via upload

---
+++ Orientação-a-Objetos /1 Bimestre/aula.php
@@ -0,0 +1,42 @@
+<?php
+
+class Monitor {
+
+    //Atributos
+    public $cor;
+    public $resolucao;
+    public $marca;
+
+    //Método
+    function ligar () {
+        echo "Computador ligado!!!\n";
+    }
+    function desligar () {
+        echo "Computador desligado!!!\n";
+    }
+    function mostrarImagem () {
+        echo "Computador mostrando imagem com a resolução $this->resolucao!!!\n";
+    }
+    
+}
+
+//Programa Principal
+$monitor1 = new Monitor();
+$monitor1 ->cor = "Preto";
+$monitor1 ->resolucao = "1080x1920";
+$monitor1 ->marca = "AOC";
+$monitor1 ->ligar();
+$monitor1 ->mostrarImagem();
+$monitor1 ->desligar();
+echo "Cor do monitor 1: " . $monitor1->cor . "\n";
+
+print "\n*****\n";
+
+$monitor2 = new Monitor();
+$monitor2 ->cor = "Prata";
+$monitor2 ->resolucao = "1966x768";
+$monitor2 ->marca = "Lenovo";
+$monitor2 ->ligar();
+$monitor2 ->mostrarImagem();
+$monitor2 ->desligar();
+echo "Cor do monitor 2: " . $monitor2->cor . "\n";
